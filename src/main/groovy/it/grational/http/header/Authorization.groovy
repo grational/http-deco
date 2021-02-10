83e1d92dfc8e656af7b4a162e8077795265e9fad
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2021-02-10 23:48:36 +0100

    missing whitespace

--- src/main/groovy/it/grational/http/header/Authorization.groovy
+++ src/main/groovy/it/grational/http/header/Authorization.groovy
@@ -3,7 +3,7 @@ package it.grational.http.header
 class Authorization extends BasicAuthentication {
 	Authorization(Map params) {
 		this.username = params.username ?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid username parameter") }()
-		this.password = params.password ?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid password parameter")}()
+		this.password = params.password ?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid password parameter") }()
 	}
 
 	@Override
