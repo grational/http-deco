28cf40d61afe4ce2340c7d23d45674bf51d3fdb2
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2023-04-19 10:05:02 +0200

    Updating the cache-containers to release-1.1.1

--- lib/src/main/groovy/it/grational/http/header/Authorization.groovy
+++ lib/src/main/groovy/it/grational/http/header/Authorization.groovy
@@ -2,8 +2,10 @@ package it.grational.http.header
 
 class Authorization extends BasicAuthentication {
 	Authorization(Map params) {
-		this.username = params.username ?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid username parameter") }()
-		this.password = params.password ?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid password parameter") }()
+		this.username = params.username 
+			?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid username parameter") }()
+		this.password = params.password 
+			?: { throw new IllegalArgumentException("[${this.class.simpleName}] Invalid password parameter") }()
 	}
 
 	@Override
