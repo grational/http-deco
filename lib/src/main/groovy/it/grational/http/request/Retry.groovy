c7e0399a1f6611f930cdb37b92e908204d086ced
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2023-05-18 12:30:42 +0200

    Retry: changing the type of the operation from closure to lamba to increase the compatibility with java

--- lib/src/main/groovy/it/grational/http/request/Retry.groovy
+++ lib/src/main/groovy/it/grational/http/request/Retry.groovy
@@ -1,17 +1,18 @@
 package it.grational.http.request
 
 import it.grational.http.response.HttpResponse
+import java.util.function.BiConsumer;
 
 class Retry implements HttpRequest {
 
 	private final HttpRequest origin
 	private final Integer     retries
-	private final Closure     retryOperation
+	private final BiConsumer retryOperation
 
 	Retry (
 		HttpRequest org,
 		Integer retries = 3,
-		Closure rop = { curr, tot -> Thread.sleep(1000 * curr) }
+		BiConsumer rop = { curr, tot -> Thread.sleep(1000 * curr) }
 	) {
 		this.origin = org
 		this.retries = retries
@@ -24,8 +25,9 @@ class Retry implements HttpRequest {
 			try {
 				HttpResponse response = this.origin.connect()
 				if ( response.error() ) {
-					if ( !retry(time) )
+					if ( !retry(time) ) {
 						raiseException(response.exception())
+					}
 				} else {
 					return response
 				}
@@ -38,8 +40,9 @@ class Retry implements HttpRequest {
 
 	private Boolean retry(Integer time) {
 		Boolean retry = (time < retries)
-		if ( retry )
-			this.retryOperation.call(time, retries)
+		if ( retry ) {
+			this.retryOperation.accept(time, retries)
+		}
 		return retry
 	}
 
